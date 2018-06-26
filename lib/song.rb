2a009ed1f0d40cca177ac6d1f33ac5c3e1052f34
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-26 03:15:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,22 +4,19 @@ class Song
   extend Memorable::ClassMethods
   extend Findable
   include Paramable
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    self.class.all << self
-  end
+  #def initialize
+  #  self.class.all << self
+#  end
 
   def artist=(artist)
     @artist = artist
   end
-
-  #def to_param
-#  #  name.downcase.gsub(' ', '-')
-#  end
 
   def self.all
     @@songs
